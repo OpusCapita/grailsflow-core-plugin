e298b066552401652105c553726e723a82ba32d0
Tanya Suhoverhaya <tanyas@scand.com>
2017-08-14 14:56:07 +0300

    (PROC-5243) Refactored code of saving ProcessNode variables

--- grails-app/services/com/jcatalog/grailsflow/ProcessVariableService.groovy
+++ grails-app/services/com/jcatalog/grailsflow/ProcessVariableService.groovy
@@ -3,7 +3,6 @@ package com.jcatalog.grailsflow
 import com.jcatalog.grailsflow.model.process.BasicProcess
 import com.jcatalog.grailsflow.model.process.ProcessNode
 import com.jcatalog.grailsflow.model.process.ProcessVariable
-import org.apache.commons.collections.ListUtils
 
 class ProcessVariableService {
 
@@ -16,36 +15,24 @@ class ProcessVariableService {
      * @param variableName - name of new variable
      * @param variableValue - value of new variable
      *
-     * @return ProcessVariable - when variable stored successfully, null - otherwise
+     * @return new ProcessVariable
      */
     public ProcessVariable saveProcessVariable(BasicProcess process, String variableName, def variableValue) {
 
-        if (!process || !variableName || !variableValue) {
-            return null
-        }
+        Objects.requireNonNull(process, "Process can't be null")
+        Objects.requireNonNull(variableName, "ProcessVariable name can't be null")
+        Objects.requireNonNull(variableValue, "ProcessVariable value can't be null")
 
-        ProcessVariable processVariable
-        try {
-            def variableTypeName = variableValue.class.simpleName
-            def variableType = ProcessVariable.defineType(variableTypeName)
+        String variableTypeName = variableValue.class.simpleName
+        def variableType = ProcessVariable.defineType(variableTypeName)
 
-            processVariable = new ProcessVariable("process": process)
-            processVariable.name = variableName
-            processVariable.type = variableType
-            processVariable.typeName = variableTypeName
+        ProcessVariable processVariable = new ProcessVariable("process": process)
+        processVariable.name = variableName
+        processVariable.type = variableType
+        processVariable.typeName = variableType == ProcessVariable.LIST ? List.class.simpleName : variableTypeName
+        processVariable.value = variableValue
+        processVariable.save(validate: false)
 
-            if (variableType == ProcessVariable.LIST) {
-                processVariable.typeName = List.class.simpleName
-                processVariable.subTypeName = variableValue[0]?.class?.simpleName
-            }
-
-            processVariable.value = variableValue
-            processVariable.save(validate: false)
-
-        } catch (Exception ex) {
-            log.error "An error occured while saving process node variable ${variableName} with value - ${variableValue}: ${ex.toString()}"
-            return null
-        }
         return processVariable
     }
 
@@ -56,15 +43,15 @@ class ProcessVariableService {
      * @param variableName - name of new variable
      * @param variableValue - value of new variable
      *
-     * @return ProcessVariable - when variable stored successfully, null - otherwise
+     * @return new ProcessVariable
      */
     public ProcessVariable saveProcessNodeVariable(ProcessNode node, String variableName, def variableValue) {
 
-        if (!node || !variableName || !variableValue) {
-            return null
-        } else {
-            variableName = "${variableName}_${node.id}"
-        }
+        Objects.requireNonNull(node, "ProcessNode can't be null")
+        Objects.requireNonNull(variableName, "ProcessVariable name can't be null")
+        Objects.requireNonNull(variableValue, "ProcessVariable value can't be null")
+
+        variableName = "${variableName}_${node.id}"
 
         return saveProcessVariable(node.process, variableName, variableValue)
     }
@@ -79,14 +66,13 @@ class ProcessVariableService {
      */
     public List<ProcessVariable> findAllProcessNodeVariables(ProcessNode node, String variableName) {
 
-        if (!node || !variableName) {
-            return ListUtils.EMPTY_LIST
-        }
+        Objects.requireNonNull(node, "ProcessNode can't be null")
+        Objects.requireNonNull(variableName, "ProcessVariable name can't be null")
 
         List<ProcessVariable> processVariables = ProcessVariable.withCriteria {
             eq('process', node?.process)
-            like('name', "${variableName}_${node.id}%")
+            eq('name', "${variableName}_${node.id}")
         }
-        return processVariables ?: ListUtils.EMPTY_LIST
+        return processVariables
     }
 }
