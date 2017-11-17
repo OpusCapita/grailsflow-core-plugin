f92a3d1d42235a1d2bd2e93e42ec51f5cea0f8aa
July Antonicheva <jantonicheva-sc@users.noreply.github.com>
2017-11-17 09:34:49 +0300

    Update GrailsflowGrailsPlugin.groovy

--- GrailsflowGrailsPlugin.groovy
+++ GrailsflowGrailsPlugin.groovy
@@ -12,7 +12,7 @@ import com.jcatalog.grailsflow.status.ProcessStatusEnum
 import com.jcatalog.grailsflow.scheduling.triggers.ConfigurableSimpleTrigger
 
 class GrailsflowGrailsPlugin {
-    def version = '1.7.11'
+    def version = '1.7.12-SNAPSHOT'
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     def dependsOn = [quartz: "1.0.1 > *"]
