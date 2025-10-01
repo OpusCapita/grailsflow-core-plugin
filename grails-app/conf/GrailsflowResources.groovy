7e3f475a3fe0ac9537c2d9079e0daccb13b3ae10
Wojciech Strzeliński <ext-wojciech.strzelinski@opuscapita.com>
2025-10-01 21:33:52 +0200

    (EPROC-22317) chore: Replace old JQuery and JQuery-UI plugins
    Cleanup resources - no reason to override jquery-ui and jquery-theme
    
    OTHER
    EPROC-22317 JS dependency vulnerability for jQuery
    https://opuscapita.atlassian.net/browse/EPROC-22317
    EPROC-22642 PoC - vulnerable JS dependencies upgrade feasibility
    https://opuscapita.atlassian.net/browse/EPROC-22642

--- grails-app/conf/GrailsflowResources.groovy
+++ grails-app/conf/GrailsflowResources.groovy
@@ -1,24 +1,6 @@
 // Resource declarations for Resources plugin
-// This is a bit ugly, we'll find a way to make this better in future
-def plugin = grails.util.Holders.getGrailsApplication().getMainContext().getBean("pluginManager")?.getGrailsPlugin("jquery-ui")
-def jqver = plugin.instance.JQUERYUI_VERSION
 
 modules = {
-    overrides {
-        'jquery-theme' {
-            resource id:'theme',
-            url:[ plugin: 'jqueryUi', dir: 'jquery-ui/themes/ui-lightness',
-                  file:'jquery-ui-'+jqver+'.custom.css'],
-            attrs:[media:'screen, projection']
-        }
-
-
-        'jquery-ui' {
-            dependsOn 'jquery', 'jquery-theme'
-            resource id:'js', url:[plugin: 'jqueryUi', dir:'jquery-ui/js', file:"jquery-ui-${jqver}.custom.min.js"],
-            nominify: true, disposition: 'head'
-        }
-    }
     grailsflow {
         dependsOn 'jquery'
         resource url: [plugin: 'grailsflow', file: 'js/grailsflow/common.js']
